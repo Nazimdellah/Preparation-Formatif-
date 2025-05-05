71fa7d8dc06a15374675a0a5c2eb6666b8c78dde
nazim <2378684@cegepmontpetit.ca>
2025-05-05 10:40:40 -0400

    Fct: timinging

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -5,11 +5,23 @@ import { bounce, shakeX, tada } from 'ng-animate';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("shake",
+      [transition(':increment', useAnimation(shakeX, { params: { time: 2000 } })),
+
+      ]),
+  ]
 })
+
 export class AppComponent {
   title = 'ngAnimations';
+  ng_shake = 0;
 
   constructor() {
   }
+  animer1Fois() {
+    this.ng_shake++;
+  }
 }
+
