7d978c9f5aef84e7ca4b5a7934cf954580c1b8bf
nazim <2378684@cegepmontpetit.ca>
2025-05-20 20:48:08 -0400

    fct:backgroundserice

---
+++ SignalR_2-backgroundService/signalr.backend/signalr.backend/Models/Channel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace signalr.backend.Models
+{
+    public class Channel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int NbMessages { get; set; }
+    }
+}
